a8961377d737e540467561f944dbfae6ccf7e232
ajlc822 <ac163@wellesley.edu>
2025-09-13 14:54:02 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+
+
+import math
+import numpy as np
+import pandas as pd
+
+clothes = pd.read_csv("clothing.csv")
+
+
+def initialize_preferences():
+    
+    assert NotImplementedError
+
+def score_likes():
+
+    assert NotImplementedError
+
+def prediction():
+
+    assert NotImplementedError
+
+def recommend_items():
+
+    assert NotImplementedError
+
+def recommend_outfits():
+
+    assert NotImplementedError
+
+if __name__ == "__main__":
+
+    print (clothes.columns) 
+
+
+
+
